1545578c337f08979ed0d8cf91723ac7c195dd41
mparaz <mparaz@gmail.com>
2019-07-10 21:45:05 +1000

    Expose API with static content and authentication disabled.

--- src/main/java/com/example/springapigeeclient/HelloController.java
+++ src/main/java/com/example/springapigeeclient/HelloController.java
@@ -3,22 +3,27 @@ package com.example.springapigeeclient;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.reactive.function.client.WebClient;
 
-// Reference: https://www.baeldung.com/spring-webclient-oauth2
+@RestController
+public class HelloController {
 
-@Component
-public class AsyncSpringApigeeClient implements SpringApigeeClient {
+    @GetMapping("/hello")
+    public String hello() {
+        return "hey yo";
+    }
+
     private static final String clientUrl = "https://mparaz-eval-test.apigee.net/helloworld-client-credentials";
 
-    private static final Logger logger = LoggerFactory.getLogger(AsyncSpringApigeeClient.class);
+    private static final Logger logger = LoggerFactory.getLogger(HelloController.class);
 
     @Autowired
     private WebClient webClient;
 
-    @Override
-    public void authenticateAndGet() {
+
+    private void authenticateAndGet() {
 
         webClient.get()
                 .uri(clientUrl)
@@ -28,4 +33,5 @@ public class AsyncSpringApigeeClient implements SpringApigeeClient {
                         -> "Retrieved using Client Credentials Grant Type: " + helloWorld)
                 .subscribe(logger::info);
     }
+
 }
--- src/main/java/com/example/springapigeeclient/SpringApigeeClientApplication.java
+++ src/main/java/com/example/springapigeeclient/SpringApigeeClientApplication.java
@@ -1,36 +1,21 @@
 package com.example.springapigeeclient;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
-import org.springframework.security.oauth2.client.OAuth2RestTemplate;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.oauth2.client.registration.ReactiveClientRegistrationRepository;
-import org.springframework.security.oauth2.client.token.grant.client.ClientCredentialsResourceDetails;
 import org.springframework.security.oauth2.client.web.reactive.function.client.ServerOAuth2AuthorizedClientExchangeFilterFunction;
 import org.springframework.security.oauth2.client.web.server.UnAuthenticatedServerOAuth2AuthorizedClientRepository;
-import org.springframework.web.client.RestTemplate;
+import org.springframework.security.web.server.SecurityWebFilterChain;
 import org.springframework.web.reactive.function.client.WebClient;
 
 @SpringBootApplication
-public class SpringApigeeClientApplication implements CommandLineRunner {
-
-	@Autowired
-	@Qualifier("syncSpringApigeeClient")
-	private SpringApigeeClient springApigeeClient;
+public class SpringApigeeClientApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringApigeeClientApplication.class, args);
 	}
-
-	@Override
-	public void run(String... args) throws Exception {
-		springApigeeClient.authenticateAndGet();
-	}
-
 
 	// Bean definitions:
 
@@ -48,16 +33,10 @@ public class SpringApigeeClientApplication implements CommandLineRunner {
 				.build();
 	}
 
-	// Sync:
-
+	// Try configuration here
 	@Bean
-	@ConfigurationProperties("sync.apigee")
-	protected ClientCredentialsResourceDetails oAuthDetails() {
-		return new ClientCredentialsResourceDetails();
-	}
-
-	@Bean
-	protected RestTemplate restTemplate() {
-		return new OAuth2RestTemplate(oAuthDetails());
+	public SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http) {
+		http.authorizeExchange().anyExchange().permitAll();
+		return http.build();
 	}
 }
